0f1fa264dc59dbc02b14471cf3d172204f1ed55c
Olivier Dumay <olivier.dumay@etu.unistra.fr>
2025-05-31 12:32:38 +0200

    PackagePartie - Clear du main

--- src/Main.java
+++ src/Main.java
@@ -11,24 +11,6 @@ public class Main
     public static void main(String args[])
     {
         System.out.println("Bienvenue dans DOOnjon et Dragons\n");
-        Carte carte =new Carte(15,15);
-
-        Personnage perso = new Personnage("Alber Venturier", EnumClasse.CLERC, EnumRace.HUMAIN);
-
-        //System.out.println("Alber.getString() : \n");
-        //perso.getString();
-        System.out.println("alber.getEtiquette = " + perso.getEtiquette());
-
-
-        carte.ajouterGameObject(perso, 2, 2);
-
-        System.out.println("carte.getEtiquettedelaCase = " + carte.getEtiquetteDeLaCase(2,2));
-        System.out.println("carte.getCase(2,2).getGameObject().getEtiquette()= " + carte.getCase(2,2).getGameObject().getEtiquette());
-
-
-        Affichage.afficherCarte(carte);
-
-
 
 
     }
