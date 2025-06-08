71d986ac2b61dc81ecb833ad9e905596b40b88e7
Olivier Dumay <olivier.dumay@etu.unistra.fr>
2025-06-08 23:55:41 +0200

    Rendu Final

--- src/dnd/Asset.java
+++ src/dnd/Asset.java
@@ -1,10 +1,5 @@
 package dnd;
 
-import dnd.Type;
-import dnd.gameobject.ennemi.Monstre;
-import dnd.gameobject.personnage.Personnage;
-import dnd.objet.Item;
-
 public interface Asset
 {
     public String getEtiquette();
